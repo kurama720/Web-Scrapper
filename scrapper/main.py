a1301f62112cf4dfee54accd56d46e3df193995a
slava <kindeev101048@gmail.com>
2021-10-21 20:22:01 +0300

    Page scrolling implemented.
    Scrolling works properly. 100 records are written to reddit.txt file.
    
    Scrolling counter is to be optimized and refactored. For now it sums all posts on the page again and again, old ones as well, despite they were already counted.

--- scrapper/main.py
+++ scrapper/main.py
@@ -14,13 +14,14 @@ def get_data(url):
 
         post_counter = 0
         while True:
-            if post_counter >= 500:
+            if post_counter >= 2000:
                 with open('reddit_source.html', 'w', encoding='utf8') as f:
                     f.write(driver.page_source)
                 break
             else:
                 post_for_move_to = driver.find_element(By.CLASS_NAME, 'promotedlink')
                 driver.execute_script('arguments[0].scrollIntoView(true);', post_for_move_to)
+                time.sleep(1)
                 posts = driver.find_elements(By.CLASS_NAME, '_1oQyIsiPHYt6nx7VOmd1sz')
                 post_counter += len(posts)
 
@@ -33,15 +34,20 @@ def get_data(url):
 
 
 def record_data(file_path):
-    with open(file_path) as f:
+    with open(file_path, 'r', encoding='utf8') as f:
         src = f.read()
 
     soup = BeautifulSoup(src, 'lxml')
     links = soup.find_all('a', class_='SQnoC3ObvgnGjWt90zD9Z')
 
     with open('reddit-YYYYMMDDHHMM.txt', 'w') as f:
+        row_counter = 0
         for i in range(0, len(links)):
-            f.write(f"UNIQUE_ID: {uuid.uuid4()}; URL: https://www.reddit.com/{links[i].get('href')}\n")
+            if row_counter == 100:
+                break
+            else:
+                f.write(f"UNIQUE_ID: {uuid.uuid4()}; URL: https://www.reddit.com/{links[i].get('href')}\n")
+                row_counter += 1
 
 
 def main():
