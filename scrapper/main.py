4475a1ae2627426531954c281908b1bde1d57417
slava <kindeev101048@gmail.com>
2021-10-21 23:21:22 +0300

    Recording function reworked
    It is supposed to write author, karma, cake day as well.
    
    Values for author, karma, cake done is None. It is to be fixed. Some user profile pages' response is 459, a sleep  between requests needs to be included.

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,6 +1,7 @@
 import uuid
 import time
 
+import requests
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 
@@ -33,26 +34,58 @@ def get_data(url):
         driver.quit()
 
 
-def record_data(file_path):
-    with open(file_path, 'r', encoding='utf8') as f:
+user_urls_list = []
+post_urls_list = []
+
+
+def get_data_urls():
+    with open('reddit_source.html', 'r', encoding='utf8') as f:
         src = f.read()
 
     soup = BeautifulSoup(src, 'lxml')
-    links = soup.find_all('a', class_='SQnoC3ObvgnGjWt90zD9Z')
+    soup_for_post_url = BeautifulSoup(src, 'lxml')
 
-    with open('reddit-YYYYMMDDHHMM.txt', 'w') as f:
-        row_counter = 0
-        for i in range(0, len(links)):
-            if row_counter == 100:
-                break
-            else:
-                f.write(f"UNIQUE_ID: {uuid.uuid4()}; URL: https://www.reddit.com/{links[i].get('href')}\n")
-                row_counter += 1
+    item_urls = soup.find_all('a', class_='_2tbHP6ZydRpjI44J3syuqC') # link for user profile
+    post_urls = soup_for_post_url.find_all('a', class_='SQnoC3ObvgnGjWt90zD9Z')
+
+    for item in post_urls:
+        if len(post_urls_list) == 100:
+            break
+        else:
+            post_url = 'https://www.reddit.com/' + item.get('href')
+            post_urls_list.append(post_url)
+
+    for item in item_urls:
+        if len(user_urls_list) == 100:
+            break
+        else:
+            user_url = 'https://www.reddit.com/' + item.get('href')
+            user_urls_list.append(user_url)
+
+
+def get_data_to_record():
+    with open('reddit.txt', 'w', encoding='utf8') as f:
+        for i in range(0, len(post_urls_list)):
+            response = requests.get(url=user_urls_list[i])
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            try:
+                data_to_record = {
+                    'UNIQUE ID': uuid.uuid4(),
+                    'post URL': post_urls_list[i],
+                    'author': soup.find('span', class_='_1LCAhi_8JjayVo7pJ0KIh0').text,
+                    'user karma': soup.find('span', id='profile--id-card--highlight-tooltip--karma').text,
+                    'cake day': soup.find('span', id='profile--id-card--highlight-tooltip--cakeday').text,
+                }
+                f.write(f"{data_to_record}\n")
+            except Exception as ex:
+                print(ex)
 
 
 def main():
     get_data('https://www.reddit.com/top/?t=month')
-    record_data('reddit_source.html')
+    get_data_urls()
+    get_data_to_record()
 
 
 if __name__ == '__main__':
--- scrapper/selenium_driver.py
+++ scrapper/selenium_driver.py
@@ -5,6 +5,7 @@ chromedriver = 'D:\\iTechArt\\scrapper\\chromedriver.exe'
 
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-notifications')
+options.add_argument('headless')
 
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
