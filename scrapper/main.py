27483aca519351d5bd46a9d5ae583ddd48d2c70e
slava <kindeev101048@gmail.com>
2021-10-23 13:46:04 +0300

    Data records correctly.
    It works correctly. Parser finds needed info and record it.
    
    If user's profile has the 18+ limit, info returns with None. Author nickname element cannot be find by parser for some reason. It doesn't see the 'div' containing nickname.

--- scrapper/main.py
+++ scrapper/main.py
@@ -72,24 +72,27 @@ def get_data_to_record():
             session = HTMLSession()
             response = session.get(url=user_urls_list[i])
             soup = BeautifulSoup(response.content, 'lxml')
-            response_for_post = session.get(url=post_urls_list[i])
-            soup_for_post = BeautifulSoup(response_for_post.content, 'lxml')
-
+            response_post = session.get(url=post_urls_list[i])
+            soup_post = BeautifulSoup(response_post.content, 'lxml')
             try:
+                unique_id = uuid.uuid4()
+                post_url = post_urls_list[i]
+                author = None
+                user_karma = soup.find('div', class_='_3KNaG9-PoXf4gcuy5_RCVy')
+                cake_day = soup.find('span', id='profile--id-card--highlight-tooltip--cakeday')
+                number_of_comments = soup_post.find('a', class_='_1UoeAeSRhOKSNdY_h3iS1O')
+                number_of_votes = soup_post.find('div', class_='_1rZYMD_4xY3gRcSS3p8ODO')
                 data_to_record = {
-                    'UNIQUE ID': uuid.uuid4(),
-                    'post URL': post_urls_list[i],
-                    'author': soup.find('span', class_='_1LCAhi_8JjayVo7pJ0KIh0'),
-                    'user karma': soup.find('div', class_='_3KNaG9-PoXf4gcuy5_RCVy'),
-                    'cake day': soup.find('span', id='profile--id-card--highlight-tooltip--cakeday'),
-                    'post karma': None,
-                    'comment karma': None,
-                    'number of comments': soup_for_post.find('a', xp='_1UoeAeSRhOKSNdY_h3iS1O'),
-                    'number of votes': soup_for_post.find('div', class_='_1rZYMD_4xY3gRcSS3p8ODO'),
-                    'post category': None
+                    'UNIQUE ID': unique_id,
+                    'POST URL': post_url,
+                    'AUTHOR': author,
+                    'USER KARMA': None if user_karma is None else user_karma.text,
+                    'CAKE DAY': None if cake_day is None else cake_day.text,
+                    'NUMBER OF COMMENTS': None if number_of_comments is None else number_of_comments.text,
+                    'NUMBER OF VOTES': None if number_of_votes is None else number_of_votes.text,
                 }
                 f.write(f"{data_to_record}\n")
-            except Exception as ex:
+            except EOFError as ex:
                 print(ex)
 
 
@@ -101,3 +104,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
