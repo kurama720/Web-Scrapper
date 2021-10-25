455e768b3307418f8a614a25e20b72049740da68
slava <kindeev101048@gmail.com>
2021-10-25 15:04:24 +0300

    Basic logging implemented
    Logging implemented by prints. All info variables wrapped into try-blocks.
    
    The problem with MaxRetryError is to be solved. Logging is to be implemented by library logging.

--- scrapper/main.py
+++ scrapper/main.py
@@ -15,12 +15,13 @@ URL = 'https://www.reddit.com/top/?t=month'
 
 
 def get_data():
+    scroll_time_start = time.time()
     try:
         driver.get(url=URL)
 
         post_counter = 0
         while True:
-            if post_counter >= 600:
+            if post_counter >= 2000:
                 with open('reddit_source.html', 'w', encoding='utf8') as f:
                     f.write(driver.page_source)
                 break
@@ -35,6 +36,7 @@ def get_data():
         print(ex)
 
     finally:
+        print(f"Scrolling done in {time.time() - scroll_time_start} sec")
         driver.close()
         driver.quit()
 
@@ -44,6 +46,7 @@ post_urls_list = []
 
 
 def get_data_urls():
+    urls_start_time = time.time()
     with open('reddit_source.html', 'r', encoding='utf8') as f:
         src = f.read()
 
@@ -66,6 +69,7 @@ def get_data_urls():
         else:
             user_url = 'https://www.reddit.com' + item.get('href')
             user_urls_list.append(user_url)
+    print(f"Urls were saved in {time.time() - urls_start_time} sec")
 
 
 MAX_WAIT = 10
@@ -77,60 +81,87 @@ def get_data_to_record():
         while True:
             try:
                 for i in range(0, len(post_urls_list)):
+                    cycle_start_time = time.time()
                     session = HTMLSession()
                     driver.get(url=post_urls_list[i])
                     action = ActionChains(driver)
                     response_user = session.get(url=user_urls_list[i])
                     soup_user = BeautifulSoup(response_user.content, 'lxml')
-                    response_post = session.get(url=post_urls_list[i])
-                    soup_post = BeautifulSoup(response_post.content, 'lxml')
+                    time.sleep(2)
 
                     unique_id = uuid.uuid4()
                     post_url = post_urls_list[i]
-                    author = driver.find_element(By.CLASS_NAME, '_2mHuuvyV9doV3zwbZPtIPG')
-                    user_karma = soup_user.find('div', class_='_3KNaG9-PoXf4gcuy5_RCVy')
-                    cake_day = soup_user.find('span', id='profile--id-card--highlight-tooltip--cakeday')
-                    number_of_comments = soup_post.find('a', class_='_1UoeAeSRhOKSNdY_h3iS1O')
-                    number_of_votes = soup_post.find('div', class_='_1rZYMD_4xY3gRcSS3p8ODO')
-                    post_category = soup_post.find('div', class_='_2Zdkj7cQEO3zSGHGK2XnZv')
-
-                    if author:
+                    while not driver.find_element(By.CLASS_NAME, '_2mHuuvyV9doV3zwbZPtIPG'):
+                        time.sleep(0.5)
+                    else:
+                        try:
+                            author = driver.find_element(By.CLASS_NAME, '_2mHuuvyV9doV3zwbZPtIPG').text\
+                                .removeprefix('u/')
+                        except Exception as ex:
+                            author = ex
+                        try:
+                            user_karma = soup_user.find('div', class_='_3KNaG9-PoXf4gcuy5_RCVy').text
+                        except Exception as ex:
+                            user_karma = ex
+                        try:
+                            cake_day = soup_user.find('span', id='profile--id-card--highlight-tooltip--cakeday').text
+                        except Exception as ex:
+                            cake_day = ex
+                        try:
+                            number_of_comments = driver.find_element(By.CLASS_NAME, '_1UoeAeSRhOKSNdY_h3iS1O').text
+                        except Exception as ex:
+                            number_of_comments = ex
+                        try:
+                            number_of_votes = driver.find_element(By.CLASS_NAME, '_1rZYMD_4xY3gRcSS3p8ODO').text
+                        except Exception as ex:
+                            number_of_votes = ex
+                        try:
+                            post_category = driver.find_element(By.CLASS_NAME, '_2Zdkj7cQEO3zSGHGK2XnZv').text\
+                                            .removeprefix('r/')
+                        except Exception as ex:
+                            post_category = ex
                         action.move_to_element(author).perform()
-                        post_and_comment_karma = driver.find_elements(By.CLASS_NAME, '_18aX_pAQub_mu1suz4-i8j')
-                        if post_and_comment_karma:
-                            data_to_record = {
-                                'UNIQUE ID': unique_id,
-                                'POST URL': post_url,
-                                'AUTHOR': 'No such element' if author is None else author.text.removeprefix('u/'),
-                                'USER KARMA': '18+ content' if user_karma is None else user_karma.text,
-                                'CAKE DAY': '18+ content' if cake_day is None else cake_day.text,
-                                'POST KARMA': 'No such element' if post_and_comment_karma[0] is None
-                                else post_and_comment_karma[0].text,
-                                'COMMENT KARMA': 'No such element' if post_and_comment_karma[1] is None
-                                else post_and_comment_karma[1].text,
-                                'NUMBER OF COMMENTS': 'No such element' if number_of_comments is None
-                                else number_of_comments.text,
-                                'NUMBER OF VOTES': 'No such element' if number_of_votes is None
-                                else number_of_votes.text,
-                                'POST CATEGORY': 'No such element' if post_category is None
-                                else post_category.text.removeprefix('r/'),
-                            }
-                            f.write(f"{data_to_record}\n")
+                        time.sleep(2)
+                        try:
+                            post_and_comment_karma = driver.find_elements(By.CLASS_NAME, '_18aX_pAQub_mu1suz4-i8j')
+                            post_karma = post_and_comment_karma[0]
+                            comment_karma = post_and_comment_karma[1]
+                        except Exception as ex:
+                            post_karma = ex
+                            comment_karma = ex
+                        data_to_record = {
+                            'UNIQUE ID': unique_id,
+                            'POST URL': post_url,
+                            'AUTHOR': author,
+                            'USER KARMA': user_karma,
+                            'CAKE DAY': cake_day,
+                            'POST KARMA': post_karma,
+                            'COMMENT KARMA': comment_karma,
+                            'NUMBER OF COMMENTS': number_of_comments,
+                            'NUMBER OF VOTES': number_of_votes,
+                            'POST CATEGORY': post_category,
+                        }
+                        f.write(f"{data_to_record}\n")
+                        cycle_end_time = int(time.time() - cycle_start_time)
+                        print(f"{i+1} link done, Cycle time spent: {cycle_end_time} sec")
 
             except (WebDriverException, MaxRetryError) as ex:
                 if time.time() - start_time > MAX_WAIT:
                     raise ex
                 time.sleep(0.5)
             finally:
+                print(f"Recording time spent: {(time.time() - start_time)//60} min")
                 driver.close()
                 driver.quit()
 
 
 def main():
-    # get_data()
+    get_data()
     get_data_urls()
     get_data_to_record()
 
 
 if __name__ == '__main__':
+    programm_start = time.time()
     main()
+    print(f"Total time spent{(time.time() - programm_start) // 60} min")
