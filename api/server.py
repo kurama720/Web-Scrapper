339bbd7c1953124a1c1c662ea87d249f034969dd
slava <kindeev101048@gmail.com>
2021-11-07 22:41:18 +0300

    Detail page implemented.
    Path posts/<UNIQUE ID> is implemented. Api searches for the given id, returns it in on the page. If no such UNIQUE ID return 404.

--- api/server.py
+++ api/server.py
@@ -1,17 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urls import urlpatterns
 import json
 
 import scrapper.main
 
-
 FIELDS = ['UNIQUE ID', 'POST URL', 'AUTHOR', 'USER KARMA', 'CAKE DAY', 'COMMENTS NUMBER', 'VOTES NUMBER',
           'POST CATEGORY', 'POST KARMA', 'COMMENT KARMA', 'POST DATE']
+
+list1 = []
 
 
 def get_data():
     # from scrapper.main import recording_data
-    list1 = []
     for record in scrapper.main.recording_data:
         i = 0
         dict2 = {}
@@ -19,7 +18,6 @@ def get_data():
             dict2[FIELDS[i]] = record[i]
             i = i + 1
         list1.append(dict2)
-    return list1
 
 
 class Server(BaseHTTPRequestHandler):
@@ -34,24 +32,33 @@ class Server(BaseHTTPRequestHandler):
         status = 200
         content_type = 'text/html'
         response_content = ''
+        section = ''
 
-        if self.path in urlpatterns:
-            if self.path == '/posts':
-                content_type = 'text/html'
-                head = '<html><body><h1>Records list</h1>'
-                number = 1
-                section = ''
-                for data in get_data():
-                    section += f"record {number}:"
-                    section += '<p>'
-                    section += json.dumps(data)
-                    section += '</p>'
-                    number += 1
-                footer = '</body></html>'
-                response_content = head.encode() + section.encode() + footer.encode()
+        if self.path == '/posts':
+            head = '<html><body><h1>Records list</h1>'
+            number = 1
+            for data in list1:
+                section += f"record {number}:"
+                section += '<p>'
+                section += json.dumps(data)
+                section += '</p>'
+                number += 1
+            footer = '</body></html>'
+            response_content = head.encode() + section.encode() + footer.encode()
+
         else:
-            content_type = 'text/html'
-            response_content = '404 Not Found'.encode()
+            head = f'<html><body><h1>The record with the UNIQUE ID {self.path.removeprefix("/posts/")}</h1>'
+            for record in list1:
+                if self.path.removeprefix('/posts/') in record['UNIQUE ID']:
+                    section += '<p>'
+                    section += json.dumps(record)
+                    section += '</p>'
+                else:
+                    continue
+            if section == '':
+                section += '404 Not found'
+            footer = '</body></html>'
+            response_content = head.encode() + section.encode() + footer.encode()
 
         self.send_response(status)
         self.send_header('Content-type', content_type)
@@ -64,7 +71,8 @@ class Server(BaseHTTPRequestHandler):
 
 
 def main():
-    ip = '127.0.0.1'
+    get_data()
+    ip = 'localhost'
     port = 8087
     server = HTTPServer(('', port), Server)
     print('Server running on %s:%s' % (ip, port))
