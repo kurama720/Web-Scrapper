39de6339d36ea05e19eb4c4bf3c8110b0b36c694
slava <kindeev101048@gmail.com>
2021-11-13 17:59:35 +0300

    Changes from dev branch included

--- scrapper/execute.py
+++ scrapper/execute.py
@@ -1,24 +1,23 @@
 """Module for working from console. Uses standard module argparse.
-
 """
 
 import argparse
 
 # Create a parser
-PARSER = argparse.ArgumentParser(description='Executing the scrapper')
-PARSER.add_argument('-a', '--action',
-                    help='"run" - to execute the program; "file" to know the file name";'
-                         ' "rows" - to know the amount of records')
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-a', '--action', required=True, choices=['run', 'file', 'rows', 'logs'],
+                    help='<run> - to execute the program; <file> to know the file name; <rows> - to know the amount of '
+                         'records; <logs> to show logs')
 ARGS = PARSER.parse_args()
 # Catch keywords from commandline.
 if ARGS.action == 'run':
     from main import main
     main()
 elif ARGS.action == 'file':
-    with open('file_info.txt', 'r') as f:
+    with open(r'D:\iTechArt\api\file_info.txt', 'r') as f:
         print(f.readlines()[0])
 elif ARGS.action == 'rows':
-    with open('file_info.txt', 'r') as f:
+    with open(r'D:\iTechArt\api\file_info.txt', 'r') as f:
         print(f.readlines()[1])
 elif ARGS.action == 'logs':
     with open('logs.log', 'r') as f:
--- scrapper/logger.py
+++ scrapper/logger.py
@@ -18,3 +18,20 @@ def create_logger():
     logger.addHandler(fh)
     logger.addHandler(fh_console)
     return logger
+
+
+def logger_for_handlers():
+    # Create logger
+    logger = logging.getLogger('scrapper/handlers.py')
+    logger.setLevel(logging.INFO)
+    # Create handlers and a formatter for logger
+    fh = logging.FileHandler('logs.log', mode='w')
+    fh_console = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)8s --- %(message)s ' +
+                                  '(%(filename)s:%(lineno)s)', datefmt='%Y-%m-%d %H:%M:%S')
+    # Add handlers and formatter to logger
+    fh.setFormatter(formatter)
+    fh_console.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(fh_console)
+    return logger
