6d80a336fec4c44ad4fb88c2e06249b320d91a6c
slava <kindeev101048@gmail.com>
2021-11-29 16:59:02 +0300

    delete_document() reworked, API checking if doc exists reworked.
    Except simple removing a post from db, function now checks whether author has any more posts, if not it removes author as well.
    
    Instead of calling find_document() to check whether document exists or not, now it makes request and cathes TypeError exception, if TypeError sends 404.
    
    Server doesn't assign a unique id to post anymore. Now unique id is to be given in JSON to be post.
    
    Docstring is to be fixed.

--- api/server.py
+++ api/server.py
@@ -2,6 +2,9 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from typing import List
+
+from pymongo.errors import DuplicateKeyError
 
 from scrapper.logger import create_logger
 from database.db_requests import insert_document, find_document, update_document, delete_document
@@ -14,57 +17,68 @@ class Server(BaseHTTPRequestHandler):
 
     # Process GET request method
     def do_GET(self):
-        if not find_document({}, all_doc=True):
-            self.send_response(404)
-        else:
-            if self.path == '/posts':
-                self.send_response(200)
-                self.send_header('Content-type', 'text/json')
-                self.end_headers()
-                for data in find_document({}, all_doc=True):
-                    self.wfile.write(json.dumps(data).encode())
-
-            elif find_document({'_id': self.path.split('/posts/')[-1]}):
-                data = find_document({'_id': self.path.split('/posts/')[-1]})
-                self.send_response(200)
-                self.send_header('Content-type', 'text/json')
-                self.end_headers()
-                self.wfile.write(json.dumps(data).encode())
+        if self.path == '/posts/':
+            # Find all documents in db with find_document()
+            data_from_db: List[dict] = find_document({})
+            if len(data_from_db) == 0:
+                self.send_response(404)
             else:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/json')
+                self.end_headers()
+                for data in data_from_db:
+                    self.wfile.write(json.dumps(data).encode())
+        else:
+            try:
+                # Find a document with given id
+                one_post = find_document({'_id': self.path.split('/posts/')[-1]})
+                self.send_response(200)
+                self.send_header('Content-type', 'text/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(one_post).encode())
+            except TypeError:
                 self.send_response(404)
 
     # Process POST request method
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
-        content = json.loads(self.rfile.read(content_len).decode())
-        insert_document(content)
-        self.send_response(201)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        doc = find_document({'AUTHOR NAME': content['AUTHOR']})
-        output = {doc['_id']: len(find_document({}, all_doc=True))}
-        self.wfile.write(json.dumps(output).encode())
+        content: dict = json.loads(self.rfile.read(content_len).decode())
+        # Insert a document
+        try:
+            insert_document(content)
+            self.send_response(201)
+            self.send_header('Content-type', 'text/json')
+            self.end_headers()
+            # Find document with proper author name
+            doc: dict = find_document({'author_name': content['author']})
+            # Get id and row number of posted doc
+            output: dict = {doc['_id']: len(find_document({}))}
+            self.wfile.write(json.dumps(output).encode())
+        except DuplicateKeyError:
+            self.send_response(404)
 
     # Process PUT request method
     def do_PUT(self):
         content_len = int(self.headers.get('Content-Length'))
-        content = json.loads(self.rfile.read(content_len).decode())
-        if find_document({'_id': self.path.split('/posts/')[-1]}):
+        content: dict = json.loads(self.rfile.read(content_len).decode())
+        try:
+            # Update document
             update_document({'_id': self.path.split('/posts/')[-1]}, content)
             self.send_response(201)
             self.send_header('Content-type', 'text/json')
             self.end_headers()
-        else:
+        except TypeError:
             self.send_response(404)
 
     # Process DELETE request method
     def do_DELETE(self):
-        if find_document({'_id': self.path.split('/posts/')[-1]}):
+        # Delete document
+        try:
             delete_document({'_id': self.path.split('/posts/')[-1]})
             self.send_response(201)
             self.send_header('Content-type', 'text/json')
             self.end_headers()
-        else:
+        except TypeError:
             self.send_response(404)
 
 
